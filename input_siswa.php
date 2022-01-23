359291ade9f973878b2b725ab27ca24b79b96341
SulastriMeilita <94740775+SulastriMeilita@users.noreply.github.com>
2022-01-23 21:51:16 +0700

    Create input_siswa.php

---
+++ input_siswa.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>UAS PEMROGRAMAN1</title>
+</head>
+<body>
+    <h2> UAS PEMROGRAMAN1 </h2>
+    <br/>
+    <a href="index.php">KEMBALI</a>
+    <br/>
+    <br/>
+    <h3> INPUT DATA SISWA </h3>
+    <form method = "post" action="tambah_aksi.php">
+        <table>
+            <tr>
+                <td> Nama </td>
+                <td><input type="text" name="nama"></td>
+            </tr>
+            <tr>
+                <td> Jenis kelamin </td>
+                <td><input type="text" name="jenis_kelamin"></td>
+            </tr>
+            <tr>
+                <td> Hobby</td>
+                <td><input type="text" name="hobby"></td>
+            </tr>
+            <tr>
+                <td> Alamat </td>
+                <td><input type="text" name="alamat"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" value="UPDATE"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
