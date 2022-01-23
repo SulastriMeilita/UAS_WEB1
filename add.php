4cb3f04d596a84c7cdd4e321d6f72a3c197492d7
SulastriMeilita <94740775+SulastriMeilita@users.noreply.github.com>
2022-01-23 21:49:52 +0700

    Create add.php

---
+++ add.php
@@ -0,0 +1,25 @@
+<a herf="index.php"> DATA DIRI </a>
+<br>
+<form action="save.php" method="post">
+<table width="25%" border="0">
+  <tr>
+    <td>Nama</td>
+    <td> <input type="text" name="nama"></td>
+  </tr>
+  <tr>
+    <td>Jenis Kelamin</td>
+    <td> <input type="text" name="jenis_kelamin"></td>
+  </tr>
+  <tr>
+    <td>Hobby</td>
+    <td> <input type="text" name="hobby"></td>
+  </tr>
+  <tr>
+    <td>Alamat</td>
+    <td> <input type="text" name="alamat"></td>
+  </tr>
+  <tr>
+    <td> <input type="submit" name="name" value="save"></td>
+  </tr>
+</table>
+</form>
